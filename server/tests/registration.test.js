76df425456f52f76b708b02debaa8c55b1d0f846
Damaris Santiago <des538@nyu.edu>
2021-10-20 11:30:33 -0400

    Linted server code and added JSDoc to sum.js

--- server/tests/registration.test.js
+++ server/tests/registration.test.js
@@ -5,7 +5,7 @@ describe('insert', () => {
   let db;
 
   beforeAll(async () => {
-    connection = await MongoClient.connect("mongodb+srv://AshaAdmin:seniordesign@ashamusic.znhbd.mongodb.net/AshaMusic?retryWrites=true&w=majority", {
+    connection = await MongoClient.connect('mongodb+srv://AshaAdmin:seniordesign@ashamusic.znhbd.mongodb.net/AshaMusic?retryWrites=true&w=majority', {
       useNewUrlParser: true,
     });
     db = await connection.db('AshaMusic');
@@ -19,82 +19,52 @@ describe('insert', () => {
   it('should insert a doc into collection', async () => {
     const users = db.collection('users');
     const mockUser = {
-        _id: "test-user-id",
-        username: "testUser",
-        password: "Passord",
-        firstName: "Test",
-        lastName: "User",
-        email: "test@user.com",
-        dob: "10/13/2020",
-        followers: [],
-        following: [],
-        bio: '',
-        genres: [],
-        isArtist: false,
-        artistName: '',
-        songs: [],
-        albums: [],
-        playlist: [{
-            'name': '',
-            'coverURL': '',
-            'songs': [],
-        }],
+      _id: 'test-user-id',
+      username: 'testUser',
+      password: 'Passord',
+      firstName: 'Test',
+      lastName: 'User',
+      email: 'test@user.com',
+      dob: '10/13/2020',
+      followers: [],
+      following: [],
+      bio: '',
+      genres: [],
+      isArtist: false,
+      artistName: '',
+      songs: [],
+      albums: [],
+      playlist: [{
+        'name': '',
+        'coverURL': '',
+        'songs': [],
+      }],
     };
 
     await users.insertOne(mockUser);
     const insertedUser = await users.findOne({
-        _id: "test-user-id",
-        username: "testUser",
-        password: "Passord",
-        firstName: "Test",
-        lastName: "User",
-        email: "test@user.com",
-        dob: "10/13/2020",
-        followers: [],
-        following: [],
-        bio: '',
-        genres: [],
-        isArtist: false,
-        artistName: '',
-        songs: [],
-        albums: [],
-        playlist: [{
-            'name': '',
-            'coverURL': '',
-            'songs': [],
-        }],
+      _id: 'test-user-id',
+      username: 'testUser',
+      password: 'Passord',
+      firstName: 'Test',
+      lastName: 'User',
+      email: 'test@user.com',
+      dob: '10/13/2020',
+      followers: [],
+      following: [],
+      bio: '',
+      genres: [],
+      isArtist: false,
+      artistName: '',
+      songs: [],
+      albums: [],
+      playlist: [{
+        'name': '',
+        'coverURL': '',
+        'songs': [],
+      }],
     });
     expect(insertedUser).toEqual(mockUser);
     await users.deleteOne(mockUser);
   });
 });
-
-// const registration = require('./../client/components/registration');
-
-// test('Testing if user can register', () => {
-//     const object = {
-//         username: "testUser",
-//         password: "Passord",
-//         firstName: "Test",
-//         lastName: "User",
-//         email: "test@user.com",
-//         dob: "10/13/2020",
-//         followers: [],
-//         following: [],
-//         bio: '',
-//         genres: [],
-//         isArtist: false,
-//         artistName: '',
-//         songs: [],
-//         albums: [],
-//         playlist: [{
-//           'name': '',
-//           'coverURL': '',
-//           'songs': [],
-//         }],
-//     };
-// });
-
-// // axios
-// //     .post("http://localhost:5000/user/register", newUser)
-// //     .then((res) => console.log(res.data));
