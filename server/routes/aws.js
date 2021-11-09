bd1929cfb3ae11e00174af9172fd02719a83b2c8
Abed Islam <ai1138@nyu.edu>
2021-11-09 15:21:03 -0500

    node modules

--- server/routes/aws.js
+++ server/routes/aws.js
@@ -9,8 +9,19 @@ const userRoutes = express.Router();
 // This will help us connect to the database
 const dbo = require('../db/conn');
 
+// Load the SDK for JavaScript
+var AWS = require('aws-sdk');
+// Set the Region 
+AWS.config.update({region: 'us-east-2'});
+
+
 // This help convert the id from string to ObjectId for the _id.
 const ObjectId = require('mongodb').ObjectId;
+
+
+
+
+
 
 
 
