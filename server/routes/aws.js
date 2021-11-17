5684c6985f206ce4c83a1c2200c721b564ba589e
Abed Islam <ai1138@nyu.edu>
2021-11-17 01:29:27 -0500

    Update aws.js
    trying aws

--- server/routes/aws.js
+++ server/routes/aws.js
@@ -1,10 +1,16 @@
 const express = require('express');
+const fs = require('fs')
+const util = require('util')
+const unlinkFile = util.promisify(fs.unlink)
+const multer = require('multer')
+const upload = multer({ dest: 'uploads/' })
+const { uploadFile } = require('./s3')
 
 // userRoutes is an instance of the express router.
 // We use it to define our routes.
 // The router will be added as a middleware and will
 // take control of requests starting with path /user.
-const userRoutes = express.Router();
+const s3routes = express.Router();
 
 // This will help us connect to the database
 const dbo = require('../db/conn');
@@ -14,11 +20,29 @@ var AWS = require('aws-sdk');
 // Set the Region 
 AWS.config.update({region: 'us-east-2'});
 
+const bucketName = "myashamusic"
+ 
+function uploadFile()
+{
+  const fileStream = fs.createReadStream(file.path)
 
-// This help convert the id from string to ObjectId for the _id.
+  const uploadParams = {
+    Bucket: bucketName,
+    Body: fileStream,
+  }
+
+  return s3.upload(uploadParams).promise()
+}
 const ObjectId = require('mongodb').ObjectId;
 
-
+s3Routes.post('/uploadTrack', upload.single('music'), async (req, res) => {
+    const file = req.file
+    console.log(file)  
+    const result = await uploadFile(file)
+    await unlinkFile(file.path)
+    console.log(result)
+    const description = req.body.description
+})
 
 
 
